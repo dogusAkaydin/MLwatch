8bc7460eb3d8741035955535324dcfeedcdfa8dd
Ubuntu <ubuntu@ip-10-0-0-13.us-west-2.compute.internal>
2018-05-14 07:45:59 +0000

    Cassandra setup

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 MODEL_DIR = './ml_model'
 IMAGES_DIR= '/home/ubuntu/ILSVRC/Data/DET/test'
-
 KAFKA_CONFIG = {
                 'topic'  : 'demo',
                 'brokers': 'localhost:9092'
                }
-
 KEYSPACE = "inception_v3_top1"
 
