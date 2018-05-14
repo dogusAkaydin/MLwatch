7cf2acfb50362ec7057e140c84ed80b6f3acd409
Ubuntu <ubuntu@ip-10-0-0-13.us-west-2.compute.internal>
2018-05-14 07:41:47 +0000

    ignored items

--- ignore/readHDFS.py
+++
@@ -1,7 +0,0 @@
-from pyspark import SparkContext, SparkConf
-
-conf = SparkConf().setAppName("myFirstApp").setMaster("local")
-sc = SparkContext(conf=conf)
-
-textFile = sc.textFile("hdfs://localhost/examples/foo.txt")
-textFile.first()
